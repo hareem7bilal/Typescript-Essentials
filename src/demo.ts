e92ad2c5d1aea36c729401be1b2b9fde60d368e4
Hareem Bilal <91612899+hareem7bilal@users.noreply.github.com>
2024-07-20 09:36:51 -0700

    defining more complex types

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,3 @@
-let x: number = 5
-let y: string
-let z: boolean
 let a: Date
 let b: any
 let m: string[]
@@ -12,9 +9,10 @@ b = 1234
 interface Contact extends Address {
     id: number,
     name: ContactName,
-    birthDate?: Date,
+    birthDate?: ContactBirthDate,
     status?: ContactStatus,
     clone?(name: string): Contact
+    address?: Address
 }
 
 interface Address {
@@ -25,32 +23,41 @@ interface Address {
     postalCode?: string;
 }
 
-enum ContactStatus {
-    Active = "active", InActive="inactive", New = "new"
-}
+// enum ContactStatus {
+//     Active = "active", InActive="inactive", New = "new"
+// }
+
+type ContactStatus = "active" | "inactive" | "new"
 
 let primaryContact: Contact = {
     //birthDate: new Date("01-01-1980"),
     id: 12345,
     name: "Jamie Johnson",
     postalCode: "44200",
-    status: ContactStatus.Active
+    status: "active"
 }
 
 type ContactName = string
+type ContactBirthDate = Date | String | Number
 
-function clone(source: Contact, func?: (source:Contact)=>Contact):Contact {
+// interface AddressableContact extends Contact, Address {
+
+// }
+
+type AddressableContact = Contact & Address
+
+function clone(source: Contact, func?: (source: Contact) => Contact): Contact {
     return Object.apply({}, source);
 }
 
 const j: Contact = { id: 123, name: "Homer Simpson" };
 const o = clone(j)
 
-function clone2<T>(source: T, func?: (source:T)=>T):T {
+function clone2<T>(source: T, func?: (source: T) => T): T {
     return Object.apply({}, source);
 }
 
-function clone3<T1,T2>(source: T1, func?: (source:T1)=>T2):T2 {
+function clone3<T1, T2>(source: T1, func?: (source: T1) => T2): T2 {
     return Object.apply({}, source);
 }
 
@@ -60,7 +67,7 @@ interface UserContact {
     username: string
 }
 
-function clone4<T1,T2 extends T1>(source: T1, func?: (source:T1)=>T2):T2 {
+function clone4<T1, T2 extends T1>(source: T1, func?: (source: T1) => T2): T2 {
     return Object.apply({}, source);
 }
 
@@ -77,3 +84,118 @@ interface UserContact2<TExternalId> {
     externalId: TExternalId
     //loadExternalId(): Task<TExternalId>
 }
+
+function getBirthDate(contact: Contact) {
+    if (typeof contact.birthDate === "number") {
+        return new Date(contact.birthDate);
+    }
+    else if (typeof contact.birthDate === "string") {
+        return Date.parse(contact.birthDate)
+    }
+    else {
+        return contact.birthDate
+    }
+}
+
+type ContactFields = keyof Contact
+
+const field: ContactFields = "status"
+
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+    return source[propertyName]
+}
+
+const value = getValue({ min: 1, max: 34 }, "max")
+
+function toContact(nameOrContact: Contact): Contact {
+    if (typeof nameOrContact === "object") {
+        return {
+            id: nameOrContact.id,
+            name: nameOrContact.name,
+            status: nameOrContact.status
+        }
+    }
+    else {
+        return {
+            id: 0,
+            name: nameOrContact,
+            status: "active"
+        }
+    }
+}
+
+const myType = { min: 1, max: 200 }
+function save(source: typeof myType){}
+
+type Awesome = Contact['address']["postalCode"]
+
+interface ContactEvent {
+    contactId: Contact["id"];
+}
+
+interface ContactDeletedEvent extends ContactEvent { 
+}
+
+interface ContactStatusChangedEvent extends ContactEvent { 
+    oldStatus: Contact["status"];
+    newStatus: Contact["status"] ;
+}
+
+interface ContactEvents {
+    deleted: ContactDeletedEvent;
+    statusChanged: ContactStatusChangedEvent;
+    // ... and so on
+}
+
+function handleEvent<T extends keyof ContactEvents>(
+    eventName: T,
+    handler: (evt: ContactEvents[T]) => void
+) {
+    if (eventName === "statusChanged") {
+        handler({ contactId: 1, oldStatus: "active", newStatus: "inactive" })
+    }
+}
+
+handleEvent("statusChanged", evt => evt)
+
+////////////////////
+
+let x: Record<string, string | number | boolean | Function> = { name: "Wruce Bayne" }
+x.number = 1234
+x.active = true
+x.log = () => console.log("awesome!")
+
+interface Query {
+    sort?: 'asc' | 'desc';
+    matches(val): boolean;
+}
+
+// type ContactQuery = Omit<Partial<Record<keyof Contact, Query>>, "address" | "status">
+type ContactQuery = Partial<Pick<Record<keyof Contact, Query>, "id"| "name">>
+type RequiredContactQuery = Required<ContactQuery>
+
+function searchContacts(contacts: Contact[], query: ContactQuery) {
+    return contacts.filter(contact => {
+        for (const property of Object.keys(contact) as (keyof Contact)[]) {
+            // get the query object for this property
+            const propertyQuery = query[property];
+            // check to see if it matches
+            if (propertyQuery && propertyQuery.matches(contact[property])) {
+                return true;
+            }
+        }
+
+        return false;
+    })
+}
+
+const filteredContacts = searchContacts(
+    [/* contacts */],
+    {
+        id: { matches: (id) => id === 123 },
+        name: { matches: (name) => name === "Carol Weaver" },
+        //phoneNumber: { matches: (name) => name === "Carol Weaver" },
+    }
+);
+
+
